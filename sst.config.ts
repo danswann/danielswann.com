a9469b30e81732ef80b3aef7f819b763bffa1b55
Daniel Swann <swann.dp@gmail.com>
2023-08-27 19:13:43 -0400

    Copy develop to main (#5)
    First pass at automatic production deployment

--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,15 @@
 import { SSTConfig } from "sst";
+import { Site } from "./stacks/Site.js"
 
 export default {
   config(_input) {
     return {
       name: "danielswanncom",
-      region: "us-east-1",
+      region: "us-east-2",
+      profile: process.env.CI ? undefined : (_input.stage === "prod" ? "production" : "development")
     };
   },
-  stacks(app) {},
+  stacks(app) {
+    app.stack(Site);
+  },
 } satisfies SSTConfig;
